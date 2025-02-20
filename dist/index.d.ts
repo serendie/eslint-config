99b16c97dc20c1c1a471b1d89445c4625c384706
nagayama <nagayama@iroirodesign.com>
2025-02-20 15:25:23 +0900

    build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,9 @@
-declare const config: any;
-export default config;
+import type { Linter } from "eslint";
+type ConfigOptions = {
+    extends?: string[];
+    rules?: Partial<Linter.RulesRecord>;
+    ignores?: string[];
+};
+export declare function createConfig(options?: ConfigOptions): any;
+declare const _default: any;
+export default _default;
--- dist/index.js
+++ dist/index.js
@@ -6,38 +6,49 @@ import eslint from "@eslint/js";
 import tsESLint from "typescript-eslint";
 import { FlatCompat } from "@eslint/eslintrc";
 const compat = new FlatCompat();
-const config = tsESLint.config(...compat.extends("plugin:storybook/recommended"), {
-    plugins: {
-        "react-refresh": reactRefresh,
-        "@pandacss": pandaCss,
-        "react-hooks": reactHooks,
-    },
-    rules: {
-        ...pandaCss.configs.recommended.rules,
-        "@pandacss/file-not-included": "off",
-        "react-refresh/only-export-components": [
-            "warn",
-            { allowConstantExport: true },
-        ],
-        "@typescript-eslint/no-unused-vars": [
-            "warn",
-            {
-                argsIgnorePattern: "^_",
-                varsIgnorePattern: "^_",
-                caughtErrorsIgnorePattern: "^_",
-                destructuredArrayIgnorePattern: "^_",
-            },
-        ],
-    },
-}, eslint.configs.recommended, ...tsESLint.configs.recommended, prettierRecommended, {
-    ignores: [
-        "**/dist/",
-        "**/styled-system/",
-        "**/*.cjs",
-        "**/public/storybook/",
-        "**/.astro/",
-        "**/.storybook/",
-        "**/env.d.ts",
+// デフォルト設定を別オブジェクトとして定義
+const defaultRules = {
+    ...pandaCss.configs.recommended.rules,
+    "@pandacss/file-not-included": "off",
+    "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
     ],
-});
-export default config;
+    "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+            argsIgnorePattern: "^_",
+            varsIgnorePattern: "^_",
+            caughtErrorsIgnorePattern: "^_",
+            destructuredArrayIgnorePattern: "^_",
+        },
+    ],
+};
+const defaultIgnores = [
+    "**/dist/",
+    "**/styled-system/",
+    "**/*.cjs",
+    "**/public/storybook/",
+    "**/.astro/",
+    "**/.storybook/",
+    "**/env.d.ts",
+];
+// カスタマイズ可能な設定を生成する関数
+export function createConfig(options) {
+    const { extends: extendConfigs = [], rules = {}, ignores = [], } = options ?? {};
+    return tsESLint.config(...compat.extends("plugin:storybook/recommended", ...extendConfigs), {
+        plugins: {
+            "react-refresh": reactRefresh,
+            "@pandacss": pandaCss,
+            "react-hooks": reactHooks,
+        },
+        rules: {
+            ...defaultRules,
+            ...rules, // カスタムルールで上書き
+        },
+    }, eslint.configs.recommended, ...tsESLint.configs.recommended, prettierRecommended, {
+        ignores: [...defaultIgnores, ...ignores], // カスタム無視パターンを追加
+    });
+}
+// デフォルト設定をエクスポート
+export default createConfig();
